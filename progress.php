23bc586a093d37b12ad5bf4b97b00991ebbdfd4e
Don McCurdy <don.r.mccurdy@gmail.com>
2012-10-31 10:07:15 +0400

    Some kind of change to progress.php

---
+++ progress.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<title>Achieving Goals to Change Behaviors - CSE556 HCI, Fall 2012</title>
+	<?php require_once('./assets/php/includes.php'); ?>
+</head>
+<body>
+	<div data-role="page">
+		<header data-role="header">
+			<h1>
+				Weekly Progress
+			</h1>
+		</header>
+		<section data-role="content">
+			[empty page]
+		</section>
+		<footer data-role="footer">
+			<h1>
+				progress.php
+			</h1>
+		</footer>
+	</div>
+</body>
+</html>
